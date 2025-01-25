f02d69b29ad4521f48e1bc77b3dabdf44cdf0f27
ayasiba <167832748+ayasiba@users.noreply.github.com>
2025-01-26 04:12:17 +0900

    Update ex02-3.js

--- ex02-3.js
+++ ex02-3.js
@@ -1,2 +1,2 @@
-function imgchange3(){document.getElementById('logo').src='./image/josaimoji_logo.png';}
-function imgchange4(){document.getElementById('logo').src='./image/josai Logo.jpg';}
+function imgchange3(){document.getElementById('logo').src='./image/';}
+function imgchange4(){document.getElementById('logo').src='./image/';}
